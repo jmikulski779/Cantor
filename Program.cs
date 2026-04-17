5be5baf9fc7cd97abdef0a43e43688c9642e7711
Jakub Mikulski <137002792+Cukerr3@users.noreply.github.com>
2026-04-17 18:45:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Projekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
